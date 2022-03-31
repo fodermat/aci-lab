ef800ca86471175e6b1a29e17778d8a61efd361d
fodermat <89015383+fodermat@users.noreply.github.com>
2022-03-31 13:45:24 +0200

    Update contracts.tf

--- modules/Contracts/contracts.tf
+++ modules/Contracts/contracts.tf
@@ -17,7 +17,7 @@ provider "aci" {
 }
 
 module "epgs" {
-   source = "https://github.com/fodermat/aci-lab/tree/main/modules/EPGs/"
+   source = "../../modules/EPGs/"
 }
 
 resource "aci_filter" "tf_icmp" {
