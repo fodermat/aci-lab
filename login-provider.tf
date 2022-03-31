6bedd6543bb9e749215166b3c1a4af4e814ab578
fodermat <89015383+fodermat@users.noreply.github.com>
2022-03-31 11:24:36 +0200

    Delete login-provider.tf

--- login-provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aci" {
-  username = "${var.apic_username}"
-  password = "${var.apic_password}"
-  #private_key = "/Users/fodermat/Terraform/fodermat-aci.key"
-  #cert_name = "/Users/fodermat/Terraform/fodermat-aci.crt"
-  url      = "${var.apic_url}"
-  insecure = true
-}
