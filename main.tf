32847ae15a38ec22a8fb6191e4101fbd4173617f
fodermat <89015383+fodermat@users.noreply.github.com>
2022-04-04 15:42:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,6 @@ terraform {
 provider "aci" {
   username = "${var.apic_username}"
   password = "${var.apic_password}"
-  #private_key = "/Users/fodermat/Terraform/fodermat-aci.key"
-  #cert_name = "/Users/fodermat/Terraform/fodermat-aci.crt"
   url      = "${var.apic_url}"
   insecure = true
 }
